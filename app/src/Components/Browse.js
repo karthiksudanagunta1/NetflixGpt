b0bf8c351efac7f419e3791a7832854a907bcb5b
karthik <chowdarykarthik858@gmail.com>
2024-11-14 07:03:29 +0530

    auth

--- app/src/Components/Browse.js
+++ app/src/Components/Browse.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Header from './Header';
+import {  useNavigate } from 'react-router-dom';
 
 function Browse() {
     const user = useSelector((state) => state.user);
-    console.log(user)
-
+    const navigate=useNavigate();
+    useEffect(()=>{
+        if(!user){
+         return navigate("/");
+        }
+        
+    },[user])
+    if(!user){
+        return null;
+    }
     return (
         <div>
             <Header/>
